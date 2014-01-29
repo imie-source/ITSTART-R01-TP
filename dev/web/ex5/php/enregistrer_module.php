59d4694efe151c806349ce0a4217730588f0ed71
scoudeimie <serge.coude@imie.fr>
2014-01-29 12:28:52 +0100

    Liste des éléments du tableau contenu dans $_POST Instruction foreach Savoir si une variable est de type tableau (pour le cas des modalités) : fonction is_array

--- dev/web/ex5/php/enregistrer_module.php
+++ dev/web/ex5/php/enregistrer_module.php
@@ -1,20 +1,51 @@
 <?php
 
-	$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
+/*	foreach($_POST as $etiquette => $valeur) {
+		if ($etiquette == "modalites") {
+			echo "<p>" . $etiquette . " : ";
+			for($i = 0; $i < count($valeur); $i++) {
+				echo $valeur[$i] . ",";
+			}
+			echo "</p>\n";
+		} else {	
+			echo "<p>" . $etiquette . " : " . $valeur . "</p>\n";
+		}	
+	}*/
+	
+	foreach($_POST as $etiquette => $valeur) {
+		if (is_array($valeur)) {
+			echo "<p>" . $etiquette . " : ";
+			for($i = 0; $i < count($valeur); $i++) {
+				echo $valeur[$i] . ",";
+			}
+			echo "</p>\n";
+		} else {	
+			echo "<p>" . $etiquette . " : " . $valeur . "</p>\n";
+		}	
+	}
+
+
+/*	$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
 	$niveau = isset($_POST["niveau"]) ? $_POST["niveau"] : "ND";
 	$intitule = isset($_POST["intitule"]) ? $_POST["intitule"] : "ND";
 	$duree = isset($_POST["duree"]) ? $_POST["duree"] : "ND";
 	$public = isset($_POST["public"]) ? $_POST["public"] : "ND";
 	$prerequis = isset($_POST["prerequis"]) ? $_POST["prerequis"] : "ND";
-	$modalites = isset($_POST["modalites"]) ? $_POST["modalites"] : "ND";
+	$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : "ND";
 	$programme = isset($_POST["programme"]) ? $_POST["programme"] : "ND";
-
+	
 	echo "<p>cat&eacute;gorie : $categorie</p>\n";
 	echo "<p>niveau : $niveau</p>\n";
-	echo "<p>intitul&eacute; : " . var_dump($intitule) . "</p>\n";
+	echo "<p>intitul&eacute; : " . $intitule . "</p>\n";
 	echo "<p>dur&eacute;e : $duree</p>\n";
 	echo "<p>public : $public</p>\n";
 	echo "<p>pr&eacute;-requis : $prerequis</p>\n";
-	echo "<p>modalit&eacute;s : " . var_dump($modalites) . "</p>\n";
+	echo "<p>modalit&eacute;s : ";
+	for($i = 0; $i < count($tabModalites); $i++) {
+		echo $tabModalites[$i] . ",";
+	}
+	echo "</p>\n";
 	echo "<p>programme : $programme</p>\n";
+	
+	*/
 ?>
