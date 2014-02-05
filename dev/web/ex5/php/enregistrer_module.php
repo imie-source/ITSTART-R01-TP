bccaf78cd2fa7407b8f516b816c8a80c08d1f0f5
scoudeimie <serge.coude@imie.fr>
2014-02-05 15:49:01 +0100

    Changement du lien pointant vers module.html vers module.php Insertion des valeurs des champs dans les tables Module et Module_has_Modalite

--- dev/web/ex5/php/enregistrer_module.php
+++ dev/web/ex5/php/enregistrer_module.php
@@ -12,7 +12,7 @@
 		}	
 	}*/
 	
-	foreach($_POST as $etiquette => $valeur) {
+	/*foreach($_POST as $etiquette => $valeur) {
 		if (is_array($valeur)) {
 			echo "<p>" . $etiquette . " : ";
 			for($i = 0; $i < count($valeur); $i++) {
@@ -23,29 +23,78 @@
 			echo "<p>" . $etiquette . " : " . $valeur . "</p>\n";
 		}	
 	}
+	*/
 
-
-/*	$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
+	// Partie définition des variables ultérieurement utilisées
+	$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
 	$niveau = isset($_POST["niveau"]) ? $_POST["niveau"] : "ND";
 	$intitule = isset($_POST["intitule"]) ? $_POST["intitule"] : "ND";
+	$objectifs = isset($_POST["objectifs"]) ? $_POST["objectifs"]) : "ND";
 	$duree = isset($_POST["duree"]) ? $_POST["duree"] : "ND";
 	$public = isset($_POST["public"]) ? $_POST["public"] : "ND";
 	$prerequis = isset($_POST["prerequis"]) ? $_POST["prerequis"] : "ND";
 	$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : "ND";
 	$programme = isset($_POST["programme"]) ? $_POST["programme"] : "ND";
 	
-	echo "<p>cat&eacute;gorie : $categorie</p>\n";
-	echo "<p>niveau : $niveau</p>\n";
-	echo "<p>intitul&eacute; : " . $intitule . "</p>\n";
-	echo "<p>dur&eacute;e : $duree</p>\n";
-	echo "<p>public : $public</p>\n";
-	echo "<p>pr&eacute;-requis : $prerequis</p>\n";
-	echo "<p>modalit&eacute;s : ";
-	for($i = 0; $i < count($tabModalites); $i++) {
-		echo $tabModalites[$i] . ",";
-	}
-	echo "</p>\n";
-	echo "<p>programme : $programme</p>\n";
+	// Enregistrement des données dans la base de données
+	/* Définition des variables utiles à la connexion à la base de données*/
+	$utilisateur = "modules";
+	$motdepasse = "modules2014!";
+	$hote = "127.0.0.1";
+	$port = 3306;
+	$nomBase = "modules";
 	
-	*/
+	/* Connexion à la base de données */
+	$link = @mysql_connect($hote.":".$port, $utilisateur, $motdepasse);
+		
+	/* Si la connexion ne s'effectue pas */
+	if (!$link) {
+		// On arrête le script et on affiche l'erreur
+		die("Impossible de se connecter &agrave; la base : " . mysql_error());
+	}
+		
+	/* Sélection de la base de données */
+	mysql_select_db($nomBase, $link);
+		
+	/* Préparation de la requète */
+	$requete = "INSERT INTO Module \
+				(Niveau_idNiveau, Categorie_idCategorie, intitule_module, \
+				 Objectifs, duree, public, prerequis, programme) \
+				VALUES \
+				($niveau, '$categorie', '$intitule', \
+				 '$objectifs', $duree, '$public', '$prerequis', '$programme')"; 
+		
+	/* Exécution de la requète */
+	$result = mysql_query($requete, $link);
+
+	/* Si la première insertion est réalisée et qu'au moins une modalité est 
+	   associée, on crée une seconde requète pour insérer dans la table
+	   Module_has_Modalite l'ensemble des modalités liées au module
+	 */
+	if ($result && count($tabModalites) > 0) {
+		/* Je récupère la clé primaire de l'enregistrement inséré dans la table
+		   Module */
+		$idModule = mysql_insert_id($link);
+		/* J'insère chaque relation entre le module et les modalités sous forme
+		   d'enregistrements dans la table Module_has_Modalite */
+		$requete = "INSERT INTO Module_has_Modalite \
+					(Modalite_idModalite, Module_idModule) VALUES ";
+		for($i = 0; $i < count($tabModalites); $i++) {
+			$requete .= "(" . $tabModalites[$i] . ", $idModule)" . ($i < count($tabModalites)-1 ? "," : "");
+		}
+		/* Exécution de la requète */
+		$result = mysql_query($requete, $link);
+		if ($result) {
+		
+		} else {
+			die("Erreur au moment de l'insertion : " . mysql_error()); 
+		}
+		
+	} else {
+		die("Erreur au moment de l'insertion : " . mysql_error()); 
+	}
+		
+	/* Déconnexion de la base */
+	mysql_close($link);
+	
 ?>
--- dev/web/ex5/php/module.php
+++ dev/web/ex5/php/module.php
@@ -1,11 +1,11 @@
 <?php
 	
 	include("fonctions.inc.php");
-
+ 
 	$lesCategories = createOptionsFromTable("Categorie", "idCategorie", "categorie_libelle"); 
 	$lesNiveaux = createOptionsFromTable("Niveau", "idNiveau", "niveau_libelle"); 
 	$lesModalites = createOptionsFromTable("Modalite", "idModalite", "modalite_libelle"); 
-	
+
 	include("../html/module.html");
 	
 ?>
