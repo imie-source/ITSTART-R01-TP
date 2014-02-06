c26612bbf2cdce1680f7eae26b6d21575c0f8f5e
scoudeimie <serge.coude@imie.fr>
2014-02-06 10:32:43 +0100

    Enregistrement des données dans les tables Module et Module_has_Modalite Optimisation du script pour éviter les redondances de code

--- dev/web/ex5/php/enregistrer_module.php
+++ dev/web/ex5/php/enregistrer_module.php
@@ -29,11 +29,11 @@
 	$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
 	$niveau = isset($_POST["niveau"]) ? $_POST["niveau"] : "ND";
 	$intitule = isset($_POST["intitule"]) ? $_POST["intitule"] : "ND";
-	$objectifs = isset($_POST["objectifs"]) ? $_POST["objectifs"]) : "ND";
+	$objectifs = isset($_POST["objectifs"]) ? $_POST["objectifs"] : "ND";
 	$duree = isset($_POST["duree"]) ? $_POST["duree"] : "ND";
 	$public = isset($_POST["public"]) ? $_POST["public"] : "ND";
 	$prerequis = isset($_POST["prerequis"]) ? $_POST["prerequis"] : "ND";
-	$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : "ND";
+	$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : array();
 	$programme = isset($_POST["programme"]) ? $_POST["programme"] : "ND";
 	
 	// Enregistrement des données dans la base de données
@@ -57,13 +57,13 @@
 	mysql_select_db($nomBase, $link);
 		
 	/* Préparation de la requète */
-	$requete = "INSERT INTO Module \
-				(Niveau_idNiveau, Categorie_idCategorie, intitule_module, \
-				 Objectifs, duree, public, prerequis, programme) \
-				VALUES \
-				($niveau, '$categorie', '$intitule', \
+	$requete = "INSERT INTO Module 
+				(Niveau_idNiveau, Categorie_idCategorie, intitule_module, 
+				 Objectifs, duree, public, prerequis, programme) 
+				VALUES 
+				($niveau, '$categorie', '$intitule', 
 				 '$objectifs', $duree, '$public', '$prerequis', '$programme')"; 
-		
+	
 	/* Exécution de la requète */
 	$result = mysql_query($requete, $link);
 
@@ -71,26 +71,36 @@
 	   associée, on crée une seconde requète pour insérer dans la table
 	   Module_has_Modalite l'ensemble des modalités liées au module
 	 */
-	if ($result && count($tabModalites) > 0) {
-		/* Je récupère la clé primaire de l'enregistrement inséré dans la table
-		   Module */
-		$idModule = mysql_insert_id($link);
-		/* J'insère chaque relation entre le module et les modalités sous forme
-		   d'enregistrements dans la table Module_has_Modalite */
-		$requete = "INSERT INTO Module_has_Modalite \
-					(Modalite_idModalite, Module_idModule) VALUES ";
-		for($i = 0; $i < count($tabModalites); $i++) {
-			$requete .= "(" . $tabModalites[$i] . ", $idModule)" . ($i < count($tabModalites)-1 ? "," : "");
-		}
-		/* Exécution de la requète */
-		$result = mysql_query($requete, $link);
-		if ($result) {
-		
-		} else {
-			die("Erreur au moment de l'insertion : " . mysql_error()); 
-		}
-		
-	} else {
+	if ($result) {
+		if (count($tabModalites) > 0) {
+			/* Je récupère la clé primaire de l'enregistrement inséré dans la table
+			   Module */
+			$idModule = mysql_insert_id($link);
+			/* J'insère chaque relation entre le module et les modalités sous forme
+			   d'enregistrements dans la table Module_has_Modalite */
+			$requete = "INSERT INTO Module_has_Modalite 
+						(Modalite_idModalite, Module_idModule) VALUES ";
+			for($i = 0; $i < count($tabModalites); $i++) {
+				$requete .= "(" . $tabModalites[$i] . ", $idModule)" . ($i < count($tabModalites)-1 ? "," : "");
+			}
+			/* Exécution de la requète */
+			$result = mysql_query($requete, $link);
+			// Si cela se passe mal...
+			if (!$result) {
+				// On affiche une erreur et on quitte le script
+				die("Erreur au moment de l'insertion : " . mysql_error()); 
+			}
+		} // if (count($tabModalites) > 0)
+		// On affiche que cela s'est bien passé et on quitte le script
+		die("<html><body>
+		     <h1 style=\"text-align: center\">Enregistrement du module 
+		     bien effectu&eacute;</h1>
+			 <p style=\"text-align: center;\">
+			 <a href=\"../html/accueil.html\">Retour</a>
+			 </p> 
+			 </body></html>");			
+	} // if ($result) { 
+	else {
 		die("Erreur au moment de l'insertion : " . mysql_error()); 
 	}
 		
