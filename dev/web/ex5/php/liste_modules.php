95268ad2df4c6b16bdb8e3f79a7d55377aa237d7
scoudeimie <serge.coude@imie.fr>
2014-02-10 15:33:09 +0100

    Ajout de "bouton" de suppression de module - utilisation de tableau HTML - définition d'une fonction appelée lors du click pour demander confirmation préalablement à la soumission du formulaire.

--- dev/web/ex5/php/liste_modules.php
+++ dev/web/ex5/php/liste_modules.php
@@ -5,7 +5,7 @@
 	
 	// Initialisation de la variable qui va contenir le code HTML de la liste
 	// des modules
-	$lesModules = "<dir>\n";
+	$lesModules = "";
 	
 	// On récupère l'ensemble des enregistrements de la table Module sous
 	// forme de tableau de tableaux associatifs
@@ -15,11 +15,15 @@
 	// qui est rajouté à la variable $lesModules encapsulé par des balises
 	// HTML de paragraphes
 	for($i = 0; $i < count($tabModules); $i++) {
-		$lesModules .= "<p><a href=\"module.php?idModule=" . $tabModules[$i]["idModule"] . "\">" . $tabModules[$i]["intitule_module"] . "</a></p>\n";
+		$lesModules .= "<tr>";
+		$lesModules .= "<td style=\"border: 1px solid black;\" >";
+		$lesModules .= "<a href=\"module.php?idModule=" . $tabModules[$i]["idModule"] . "\">" . $tabModules[$i]["intitule_module"] . "</a>";
+		$lesModules .= "</td>";
+		$lesModules .= "<td style=\"text-align: center; vertical-align: middle;\" >";
+		$lesModules .= "<img src=\"../medias/images/supprimer.gif\" onclick=\"suppression(" . $tabModules[$i]["idModule"] . ");\"/>";
+		$lesModules .= "</td>";
+		$lesModules .= "</tr>\n";
 	}
-	
-	// On ferme la balise "tabulation"
-	$lesModules .= "</dir>\n";
 	
 	// On inclue finalement le fichier HTML global
 	include("../html/liste_modules.html");
