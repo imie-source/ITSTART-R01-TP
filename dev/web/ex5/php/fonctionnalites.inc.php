30829a971f831ea69900222b0da9669c4cb85d64
scoudeimie <serge.coude@imie.fr>
2014-02-12 10:34:35 +0100

    Fin de la configuration du pilotage de l'application par index.php Suppression des anciens scripts devenus inutiles

---
+++ dev/web/ex5/php/fonctionnalites.inc.php
@@ -0,0 +1,258 @@
+<?php
+
+	include("fonctions.inc.php");
+
+	/**
+	 * Affiche un formulaire de saisie d'un module (renseigné ou non)
+	 * @param int $idModule Id du module à afficher
+	 */
+	function creerModule($idModule) {
+		// Initialisation "générale" des différentes variables présentes dans
+		// le formulaire
+		
+		$intitule = "";
+		$objectifs = "";
+		$public = "";
+		$duree = "";
+		$programme = "";
+		$prerequis = "";
+		$categorie = false;
+		$niveau = false;
+		// Comme il peut y avoir plusieurs modalités liées à un module
+		// On définit la variable $tabModalites commes un tableau
+		$tabModalites = array();
+			
+		// Si un id de module est passé en paramètre (méthode GET)
+		if ($idModule) {
+			// Récupération de l'ensemble des enregistrements des modules
+			$tabModules = getDonnees("Module");
+			// Recherche du module spécifique
+			for($i = 0; $i < count($tabModules); $i++) {
+				// L'id du module correspond-il à celui demandé ?
+				if ($tabModules[$i]["idModule"] == $idModule) {
+					// Oui, on récupère l'ensemble des informations
+					$intitule = $tabModules[$i]["intitule_module"];
+					$objectifs = $tabModules[$i]["objectifs"];
+					$public = $tabModules[$i]["public"];
+					$duree = $tabModules[$i]["duree"];
+					$programme = $tabModules[$i]["programme"];
+					$prerequis = $tabModules[$i]["prerequis"];
+					$categorie = $tabModules[$i]["Categorie_idCategorie"];
+					$niveau = $tabModules[$i]["Niveau_idNiveau"];
+					// Pas la peine de continuer, on a trouvé, donc on arrête la boucle
+					break;
+				}
+			}
+			$tabRelationModuleModalites = getDonnees("module_has_modalite");
+			// Recherche des modalités liées au module spécifique
+			// On parcourt l'ensemble des relations entre les modules et les modalités
+			for($i = 0; $i < count($tabRelationModuleModalites); $i++) {
+				// Si une relation met en jeu le module demandé
+				if ($tabRelationModuleModalites[$i]["Module_idModule"] == $idModule) {
+					// On stocke l'id de la modalité dans le tableau $tabModalites
+					$tabModalites[] = $tabRelationModuleModalites[$i]["Modalite_idModalite"];
+				}
+			}
+		}
+		// Génération des listes des "select" avec potentiellement la "sélection" d'une passée en 4ème argument
+		$lesCategories = createOptionsFromTable("Categorie", "idCategorie", "categorie_libelle", $categorie); 
+		$lesNiveaux = createOptionsFromTable("Niveau", "idNiveau", "niveau_libelle", $niveau); 
+		$lesModalites = createOptionsFromTable("Modalite", "idModalite", "modalite_libelle", $tabModalites); 
+
+		include("../html/module.html");
+	} // Fin creerModule
+	
+	/**
+	 * Insert dans la table Module_has_Modalite l'ensemble des relations entre un module
+	 * et une ou plusieurs modalités : un enregistrement par relation
+	 * @param array $tabModalites Liste des modalités en relation avec le module
+	 * @param int $idModule l'id (clé primaire) du module
+	 * @param resource $link "Ticket" de connexion à la base de données
+	 */
+	function attribueRelationsEntreModuleEtModalites($tabModalites, $idModule, $cnxPDO) {
+		/* J'insère chaque relation entre le module et les modalités sous forme
+		   d'enregistrements dans la table Module_has_Modalite */
+		$requete = "INSERT INTO Module_has_Modalite 
+					(Modalite_idModalite, Module_idModule) VALUES ";
+		for($i = 0; $i < count($tabModalites); $i++) {
+			$requete .= "(" . $tabModalites[$i] . ", $idModule)" . ($i < count($tabModalites)-1 ? "," : "");
+		}
+		
+		/* Exécution de la requète */
+		$result = $cnxPDO->exec($requete);
+		if (false === $result) {
+			// On affiche une erreur et on quitte le script
+			bddErreur(BDD_ERREUR_INSERT, $cnxPDO->errorInfo());
+		}
+	} // Fin attribueRelationsEntreModuleEtModalites
+	
+	/**
+	 * Enregistre le module dont les informations sont passées dans la variable
+	 * globale $_POST
+	 */  
+	function enregistrerModule() {
+		global $_POST;
+		// Partie définition des variables ultérieurement utilisées
+		$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
+		$niveau = isset($_POST["niveau"]) ? $_POST["niveau"] : "ND";
+		$intitule = addslashes(isset($_POST["intitule"]) ? $_POST["intitule"] : "ND");
+		$objectifs = addslashes(isset($_POST["objectifs"]) ? $_POST["objectifs"] : "ND");
+		$duree = isset($_POST["duree"]) ? $_POST["duree"] : "ND";
+		$public = addslashes(isset($_POST["public"]) ? $_POST["public"] : "ND");
+		$prerequis = addslashes(isset($_POST["prerequis"]) ? $_POST["prerequis"] : "ND");
+		$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : array();
+		$programme = addslashes(isset($_POST["programme"]) ? $_POST["programme"] : "ND");
+		$idModule = isset($_POST["idModule"]) ? $_POST["idModule"] : false;
+		
+		// Enregistrement des données dans la base de données
+		
+		$cnxPDO = cnxBase();
+		
+		if (is_string($cnxPDO)) {
+			// On arrête le script et on affiche l'erreur
+			bddErreur(BDD_ERREUR_CNX, $cnxPDO);
+		}
+		
+		if (!$idModule) { // création d'un nouveau module
+				
+			/* Préparation de la requète */
+			$requete = "INSERT INTO Module 
+						(Niveau_idNiveau, Categorie_idCategorie, intitule_module, 
+						 Objectifs, duree, public, prerequis, programme) 
+						VALUES 
+						($niveau, '$categorie', '$intitule', 
+						 '$objectifs', $duree, '$public', '$prerequis', '$programme')"; 
+		} else { // modification d'un module
+			
+		/* Préparation de la requète */
+			$requete = "UPDATE Module
+						SET Niveau_idNiveau = $niveau,
+							Categorie_idCategorie = '$categorie',
+							intitule_module = '$intitule',
+							Objectifs = '$objectifs',
+							duree = $duree,
+							public = '$public',
+							prerequis = '$prerequis',
+							programme = '$programme'
+						WHERE idModule = $idModule";		
+		
+		}	
+		
+		/* Exécution de la requète */
+		$result = $cnxPDO->exec($requete);
+
+		/* Si la première insertion ou la mise à jour  est réalisée 
+		   et qu'au moins une modalité est associée, 
+		   on crée une seconde requète pour insérer dans la table
+		   Module_has_Modalite l'ensemble des modalités liées au module
+		 */
+		if ($result !== false) {
+			if (!$idModule) { // Si on crée un module
+				/* Je récupère la clé primaire de l'enregistrement inséré dans la table
+				   Module */
+				$idModule = $cnxPDO->lastInsertId();
+			} else { // Si on met à jour un module
+				/* Suppression de l'ensemble des relations entre les modalités et 
+				   le module concerné */
+				$requete = "DELETE FROM Module_has_Modalite
+							WHERE Module_idModule = $idModule";
+							
+				/* Exécution de la requète */
+				$result = $cnxPDO->exec($requete);		
+				
+				if (false === $result) {
+					// Si la suppression des anciennes relations ne s'est pas bien passée
+					bddErreur(BDD_ERREUR_DELETE, $cnxPDO->errorInfo(), $requete); 
+				}
+			}
+			// Si au moins une modalité associée au module
+			if (count($tabModalites) > 0) {
+				attribueRelationsEntreModuleEtModalites($tabModalites, $idModule, $cnxPDO);
+			} 
+			
+			// On affiche que cela s'est bien passé et on quitte le script
+			include("../html/enregistrement_ok.html");			
+		} // Fin du cas où s'est bien passée l'insertion ou la mise à jour
+		else { // En cas d'erreur lors de l'insertion ou de la mise à jour
+			
+			if (false === $idModule) { // Cas de l'insertion
+				bddErreur(BDD_ERREUR_INSERT, $cnxPDO->errorInfo(), $requete);	
+			} else { // Cas de la mise à jour
+				bddErreur(BDD_ERREUR_UPDATE, $cnxPDO->errorInfo(), $requete);
+			}
+		}
+	} // Fin enregistrerModule
+	
+	/**
+	 * Affiche la liste des modules présents dans la base de données
+	 */
+	function listeDesModules() {
+		// Initialisation de la variable qui va contenir le code HTML de la liste
+		// des modules
+		$lesModules = "";
+		
+		// On récupère l'ensemble des enregistrements de la table Module sous
+		// forme de tableau de tableaux associatifs
+		$tabModules = getDonnees("Module", array("intitule_module", "idModule"));
+		
+		// On parcourt l'ensemble des enregistrements et on en extrait l'intitulé
+		// qui est rajouté à la variable $lesModules encapsulé par des balises
+		// HTML de paragraphes
+		for($i = 0; $i < count($tabModules); $i++) {
+			$lesModules .= "<tr>";
+			$lesModules .= "<td style=\"border: 1px solid black;\" >";
+			$lesModules .= "<a href=\"" . $_SERVER["PHP_SELF"] . "?idModule=" . $tabModules[$i]["idModule"] . "&action=creerModule\">" . $tabModules[$i]["intitule_module"] . "</a>";
+			$lesModules .= "</td>";
+			$lesModules .= "<td style=\"text-align: center; vertical-align: middle;\" >";
+			$lesModules .= "<img src=\"../medias/images/supprimer.gif\" onclick=\"suppression(" . $tabModules[$i]["idModule"] . ");\"/>";
+			$lesModules .= "</td>";
+			$lesModules .= "</tr>\n";
+		}
+		
+		// On inclue finalement le fichier HTML global
+		include("../html/liste_modules.html");
+	} // Fin listeDesModules
+	
+	/**
+	 * Supprimer le module passé en paramètre
+	 * @param int $idModule Id du module à supprimer
+	 */
+	function supprimerModule($idModule) {
+		
+		// Connexion à la base de données
+		$cnxPDO = cnxBase();
+		
+		// Gestion des erreurs éventuelles
+		if (is_string($cnxPDO)) {
+			// On arrête le script et on affiche l'erreur
+			bddErreur(BDD_ERREUR_CNX, $cnxPDO);
+		}
+		// Suppression des relations entre le module et ses modalités
+		$requete = "DELETE FROM Module_has_Modalite
+					WHERE Module_idModule = " . $idModule;
+					
+		/* Exécution de la requète */
+		$result = $cnxPDO->exec($requete);
+		
+		// Gestion des erreurs éventuelles
+		if (false === $result) {
+			bddErreur(BDD_ERREUR_DELETE, $cnxPDO->errorInfo(), $requete);
+		}					
+						
+		// Suppression du module
+		$requete = "DELETE FROM Module
+					WHERE idModule = " . $idModule;		
+					 
+		/* Exécution de la requète */
+		$result = $cnxPDO->exec($requete);
+		
+		// Gestion des erreurs éventuelles
+		if (false === $result) {
+			bddErreur(BDD_ERREUR_DELETE, $cnxPDO->errorInfo(), $requete);
+		}
+		
+		header("Location: " . $_SERVER["PHP_SELF"] . "?action=listerModule"); 
+		
+	} // Fin supprimerModule
+
+?>
--- dev/web/ex5/php/index.php
+++ dev/web/ex5/php/index.php
@@ -1,187 +1,9 @@
 <?php
 
-	include("fonctions.inc.php");
-
-	function creerModule($idModule) {
-		// Initialisation "générale" des différentes variables présentes dans
-		// le formulaire
-		
-		$intitule = "";
-		$objectifs = "";
-		$public = "";
-		$duree = "";
-		$programme = "";
-		$prerequis = "";
-		$categorie = false;
-		$niveau = false;
-		// Comme il peut y avoir plusieurs modalités liées à un module
-		// On définit la variable $tabModalites commes un tableau
-		$tabModalites = array();
-			
-		// Si un id de module est passé en paramètre (méthode GET)
-		if ($idModule) {
-			// Récupération de l'ensemble des enregistrements des modules
-			$tabModules = getDonnees("Module");
-			// Recherche du module spécifique
-			for($i = 0; $i < count($tabModules); $i++) {
-				// L'id du module correspond-il à celui demandé ?
-				if ($tabModules[$i]["idModule"] == $idModule) {
-					// Oui, on récupère l'ensemble des informations
-					$intitule = $tabModules[$i]["intitule_module"];
-					$objectifs = $tabModules[$i]["objectifs"];
-					$public = $tabModules[$i]["public"];
-					$duree = $tabModules[$i]["duree"];
-					$programme = $tabModules[$i]["programme"];
-					$prerequis = $tabModules[$i]["prerequis"];
-					$categorie = $tabModules[$i]["Categorie_idCategorie"];
-					$niveau = $tabModules[$i]["Niveau_idNiveau"];
-					// Pas la peine de continuer, on a trouvé, donc on arrête la boucle
-					break;
-				}
-			}
-			$tabRelationModuleModalites = getDonnees("module_has_modalite");
-			// Recherche des modalités liées au module spécifique
-			// On parcourt l'ensemble des relations entre les modules et les modalités
-			for($i = 0; $i < count($tabRelationModuleModalites); $i++) {
-				// Si une relation met en jeu le module demandé
-				if ($tabRelationModuleModalites[$i]["Module_idModule"] == $idModule) {
-					// On stocke l'id de la modalité dans le tableau $tabModalites
-					$tabModalites[] = $tabRelationModuleModalites[$i]["Modalite_idModalite"];
-				}
-			}
-		}
-		// Génération des listes des "select" avec potentiellement la "sélection" d'une passée en 4ème argument
-		$lesCategories = createOptionsFromTable("Categorie", "idCategorie", "categorie_libelle", $categorie); 
-		$lesNiveaux = createOptionsFromTable("Niveau", "idNiveau", "niveau_libelle", $niveau); 
-		$lesModalites = createOptionsFromTable("Modalite", "idModalite", "modalite_libelle", $tabModalites); 
-
-		include("../html/module.html");
-	}
+	include("fonctionnalites.inc.php");
 	
-	/**
-	 * Insert dans la table Module_has_Modalite l'ensemble des relations entre un module
-	 * et une ou plusieurs modalités : un enregistrement par relation
-	 * @param array $tabModalites Liste des modalités en relation avec le module
-	 * @param int $idModule l'id (clé primaire) du module
-	 * @param resource $link "Ticket" de connexion à la base de données
-	 */
-	function attribueRelationsEntreModuleEtModalites($tabModalites, $idModule, $cnxPDO) {
-		/* J'insère chaque relation entre le module et les modalités sous forme
-		   d'enregistrements dans la table Module_has_Modalite */
-		$requete = "INSERT INTO Module_has_Modalite 
-					(Modalite_idModalite, Module_idModule) VALUES ";
-		for($i = 0; $i < count($tabModalites); $i++) {
-			$requete .= "(" . $tabModalites[$i] . ", $idModule)" . ($i < count($tabModalites)-1 ? "," : "");
-		}
-		
-		/* Exécution de la requète */
-		$result = $cnxPDO->exec($requete);
-		if (false === $result) {
-			// On affiche une erreur et on quitte le script
-			bddErreur(BDD_ERREUR_INSERT, $cnxPDO->errorInfo());
-		}
-	}
-	
-	function enregistrerModule() {
-		global $_POST;
-		// Partie définition des variables ultérieurement utilisées
-		$categorie = isset($_POST["categorie"]) ? $_POST["categorie"] : "ND";
-		$niveau = isset($_POST["niveau"]) ? $_POST["niveau"] : "ND";
-		$intitule = addslashes(isset($_POST["intitule"]) ? $_POST["intitule"] : "ND");
-		$objectifs = addslashes(isset($_POST["objectifs"]) ? $_POST["objectifs"] : "ND");
-		$duree = isset($_POST["duree"]) ? $_POST["duree"] : "ND";
-		$public = addslashes(isset($_POST["public"]) ? $_POST["public"] : "ND");
-		$prerequis = addslashes(isset($_POST["prerequis"]) ? $_POST["prerequis"] : "ND");
-		$tabModalites = isset($_POST["modalites"]) ? $_POST["modalites"] : array();
-		$programme = addslashes(isset($_POST["programme"]) ? $_POST["programme"] : "ND");
-		$idModule = isset($_POST["idModule"]) ? $_POST["idModule"] : false;
-		
-		// Enregistrement des données dans la base de données
-		
-		$cnxPDO = cnxBase();
-		
-		if (is_string($cnxPDO)) {
-			// On arrête le script et on affiche l'erreur
-			bddErreur(BDD_ERREUR_CNX, $cnxPDO);
-		}
-		
-		if (!$idModule) { // création d'un nouveau module
-				
-			/* Préparation de la requète */
-			$requete = "INSERT INTO Module 
-						(Niveau_idNiveau, Categorie_idCategorie, intitule_module, 
-						 Objectifs, duree, public, prerequis, programme) 
-						VALUES 
-						($niveau, '$categorie', '$intitule', 
-						 '$objectifs', $duree, '$public', '$prerequis', '$programme')"; 
-		} else { // modification d'un module
-			
-		/* Préparation de la requète */
-			$requete = "UPDATE Module
-						SET Niveau_idNiveau = $niveau,
-							Categorie_idCategorie = '$categorie',
-							intitule_module = '$intitule',
-							Objectifs = '$objectifs',
-							duree = $duree,
-							public = '$public',
-							prerequis = '$prerequis',
-							programme = '$programme'
-						WHERE idModule = $idModule";		
-		
-		}	
-		
-		/* Exécution de la requète */
-		$result = $cnxPDO->exec($requete);
-
-		/* Si la première insertion ou la mise à jour  est réalisée 
-		   et qu'au moins une modalité est associée, 
-		   on crée une seconde requète pour insérer dans la table
-		   Module_has_Modalite l'ensemble des modalités liées au module
-		 */
-		if ($result !== false) {
-			if (!$idModule) { // Si on crée un module
-				/* Je récupère la clé primaire de l'enregistrement inséré dans la table
-				   Module */
-				$idModule = $cnxPDO->lastInsertId();
-			} else { // Si on met à jour un module
-				/* Suppression de l'ensemble des relations entre les modalités et 
-				   le module concerné */
-				$requete = "DELETE FROM Module_has_Modalite
-							WHERE Module_idModule = $idModule";
-							
-				/* Exécution de la requète */
-				$result = $cnxPDO->exec($requete);		
-				
-				if (false === $result) {
-					// Si la suppression des anciennes relations ne s'est pas bien passée
-					bddErreur(BDD_ERREUR_DELETE, $cnxPDO->errorInfo(), $requete); 
-				}
-			}
-			// Si au moins une modalité associée au module
-			if (count($tabModalites) > 0) {
-				attribueRelationsEntreModuleEtModalites($tabModalites, $idModule, $cnxPDO);
-			} 
-			
-			// On affiche que cela s'est bien passé et on quitte le script
-			die("<html><body>
-				 <h1 style=\"text-align: center\">Enregistrement du module 
-				 bien effectu&eacute;</h1>
-				 <p style=\"text-align: center;\">
-				 <a href=\"../html/accueil.html\">Retour</a>
-				 </p> 
-				 </body></html>");			
-		} // Fin du cas où s'est bien passée l'insertion ou la mise à jour
-		else { // En cas d'erreur lors de l'insertion ou de la mise à jour
-			
-			if (false === $idModule) { // Cas de l'insertion
-				bddErreur(BDD_ERREUR_INSERT, $cnxPDO->errorInfo(), $requete);	
-			} else { // Cas de la mise à jour
-				bddErreur(BDD_ERREUR_UPDATE, $cnxPDO->errorInfo(), $requete);
-			}
-		}
-	}
-	
-	
+	// Poste de pilotage
+	// On récupère l'action souhaitée (en POST ou en GET)
 	if (isset($_GET["action"])) {
 		$action = $_GET["action"];
 	} else if (isset($_POST["action"])) {
@@ -189,6 +11,7 @@
 	} else {
 		$action = false;
 	}	
+	// En fonction de l'action
 	switch($action) {
 		case "creerModule":
 			creerModule(isset($_GET["idModule"]) ? $_GET["idModule"] : false);
@@ -197,6 +20,12 @@
 			enregistrerModule($_POST);
 			break;
 		case "listerModule":
+			listeDesModules();
+			break;
+		case "supprimerModule":
+			if (isset($_POST["idModule"])) {
+				supprimerModule($_POST["idModule"]);
+			}	
 			break;
 		default:
 			include("../html/accueil.html");
