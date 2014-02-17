645644d5a4cb788e6ecb2433a35984b167b889d8
scoudeimie <serge.coude@imie.fr>
2014-02-17 11:40:38 +0100

    Définition des champs d'une voiture et des getter et setter idoînes

--- dev/web/ex6/php/class/voiture.class.php
+++ dev/web/ex6/php/class/voiture.class.php
@@ -2,30 +2,138 @@
 
 	class voiture {
 	
-		private $couleur;
-	
-		public function __construct($couleur) {
+		private $_couleur;
+		
+		private $_marque;
+		
+		private $_modele;
+		
+		private $_immatriculation;
+		
+		private $_nbPortes;
+		
+		private $_puissance;
+		
+		private $_boiteVitesse;
+		
+		private $_nbVitesses;
+		
+		private $_energie;
+		
+		const ENERGIE_ESSENCE = "essence";
+		const ENERGIE_DIESEL = "diesel";
+		const ENERGIE_GPL = "gpl";
+		const ENERGIE_ELECTRICITE = "électricité";
+		
+				
+		
+		public function __construct($couleur, $marque, $modele, $immatriculation, $nbPortes, $puissance, $boiteVitesse, $nbVitesses, $energie) {
 			echo "je suis construit, avec la couleur : $couleur !<br />";
 			$this->setCouleur($couleur);
+			$this->setMarque($marque);
+			$this->setModele($modele);
+			$this->setImmatriculation($immatriculation);
+			$this->setNbPortes($nbPortes);
+			$this->setPuissance($puissance);
+			$this->setBoiteVitesse($boiteVitesse);
+			$this->setNbVitesses($nbVitesses);
+			$this->setEnergie($energie);
 		}
 		
 		public function getCouleur() {
-			return $this->couleur;
+			return $this->_couleur;
 		}
 		
 		public function setCouleur($couleur) {
 			switch($couleur) {
 				case "noir":
 				case "rouge": 
-					$this->couleur = $couleur;
+					$this->_couleur = $couleur;
 					break;
 				default:
-					$this->couleur = "NA";
+					$this->_couleur = "NA";
 			}		
 		}
-	
+		
+		public function getMarque() {
+			return $this->_marque;
+		}
+		
+		public function setMarque($marque) {
+			$this->_marque = $marque;
+		}
+		
+		public function getModele() {
+			return $this->_modele;
+		}
+		
+		public function setModele($modele) {
+			$this->_modele = $modele;
+		}
+		
+		public function getImmatriculation() {
+			return $this->_immatriculation;
+		}
+		
+		public function setImmatriculation($immatriculation) {
+			$this->_immatriculation = $immatriculation;
+		}
+		
+		public function getNbPortes() {
+			return $this->_nbPortes;
+		}
+		
+		public function setNbPortes($nbPortes) {
+			$this->_nbPortes = $nbPortes;
+		}
+		
+		public function getPuissance() {
+			return $this->_puissance;
+		}
+		
+		public function setPuissance($puissance) {
+			$this->_puissance = $puissance;
+		}
+		
+		public function getBoiteVitesse() {
+			return $this->_boiteVitese;
+		}
+		
+		public function setBoiteVitesse($boiteVitesse) {
+			$this->_boiteVitesse = $boiteVitesse;
+		}
+		
+		public function getNbVitesses() {
+			return $this->_nbVitesses;
+		} 
+		
+		public function setNbVitesses($nbVitesses) {
+			$this->_nbVitesses = $nbVitesses;
+		}
+		
+		public function getEnergie() {
+			return $this->_energie;
+		}
+		
+		public function setEnergie($energie) {
+			$energie = strtolower($energie);
+			$energie = retireAccent($energie);
+			switch($energie) {
+				case "essence" :
+					
+				case "diesel" :
+					$this->_energie = $energie;
+					break;
+				case "electricite" :
+					
+					break;
+				default:
+					$this->_energie = "ND";
+			}
+		}	
+		
 		public function __toString() {
-			return "je suis une voiture de couleur " . $this->couleur . "<br />";
+			return "je suis une voiture de couleur " . $this->_couleur . "<br />";
 		}
 	
 	}
